ee8723354f66182b880040dfa3f8782536c5ad29
MATTHEW OLANIPEKUN <119475643+CandO01@users.noreply.github.com>
2025-07-10 03:31:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -37,9 +37,12 @@ const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
 
   if (req.method === 'OPTIONS') {
-    res.writeHead(204)
-    res.end()
-    return
+    res.writeHead(204, {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PATCH, DELETE',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    });
+    return res.end();
   }
 
   // Sign Up
